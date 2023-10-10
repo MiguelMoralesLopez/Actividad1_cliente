3956021d1adb6e9585dceb1ea4804321568958af
Miguel Morales Lopez <miguel.mlopez07@gmail.com>
2023-10-10 12:48:46 +0200

    añado funcionalidades js a los botones validar y reset

--- Requerimiento2/indexJs.js
+++ Requerimiento2/indexJs.js
@@ -17,8 +17,6 @@ function calcularPrecio(){
     nuevoP.appendChild(nuevoPrecio)
     nuevoP.id = "precio"
     formulario.appendChild(nuevoP)
-    
-    
 }
 
 function comprobarSize(){
@@ -37,6 +35,11 @@ function comprobarSize(){
 
 function comprobarIngredientes(){
     let precioIng = 0;
+
+    if((document.getElementById("pepperoni").checked == false) && (document.getElementById("bacon").checked == false)
+        && (document.getElementById("anchoas").checked == false) && (document.getElementById("huevo").checked == false))
+            alert('Debes seleccionar al menos un ingrediente.');
+    
     if(document.getElementById("pepperoni").checked)
         precioIng++
     if(document.getElementById("bacon").checked)
@@ -47,4 +50,33 @@ function comprobarIngredientes(){
         precioIng++
     
     return precioIng
+}
+
+function validarFormulario(){
+    if (nombre.value == null || nombre.value == "")
+        alert('Introduce tu nombre')
+    if (apellidos.value == null || apellidos.value == "")
+        alert('Introduce tus apellidos')
+    if (direccion.value == null || direccion.value == "")
+        alert('Introduce tu dirección')
+    if (tlf.value == null || tlf.value == "")
+        alert('Introduce tu teléfono')
+    if (email.value == null || email.value == "")
+        alert('Introduce tu email')
+    if((document.getElementById("peq").checked == false) && (document.getElementById("med").checked == false)
+        && (document.getElementById("grande").checked == false))
+            alert('Debes elegir un tamaño para la pizza')
+    if((document.getElementById("pepperoni").checked == false) && (document.getElementById("bacon").checked == false)
+        && (document.getElementById("anchoas").checked == false) && (document.getElementById("huevo").checked == false))
+            alert('Debes seleccionar al menos un ingrediente.');
+    
+}
+
+function resetPrecio(){
+    precio.parentNode.removeChild(precio);
+    let nuevoPrecio = document.createTextNode("Precio total: ")
+    let nuevoP = document.createElement("p")
+    nuevoP.appendChild(nuevoPrecio)
+    nuevoP.id = "precio"
+    formulario.appendChild(nuevoP)
 }
