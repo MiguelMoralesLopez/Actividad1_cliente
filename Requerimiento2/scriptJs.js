ba3f59268cec00155e0d15f1a858e7323b4ee307
Juanjo <sistemasjuanjo@outlook.es>
2023-10-24 10:10:40 +0200

    subo req1 y req2

--- Requerimiento2/scriptJs.js
+++ Requerimiento2/scriptJs.js
@@ -1,6 +1,18 @@
 
 
-        // funcion precio
+        /*funcion precio calcular precio
+         -pizzas: variable recibe dato desde la opcion elegida 
+         -pizzaElegida variable creada, recibe el valor de la pizza elegida
+         Se transforma a entero porque el venia con valor "String"
+         - ingredientes: Variable que recibe valor desde los checks propuestos
+         - ingredienteElegido : Variable que tras iterar añadirá el elemento en un Array
+         - ingredientePrecio : Variable con el valor y parseada a entero al venir como "String"
+         - precioTotal : resultado de la obtencion de la pizza eleguida mas los ingredientes seleccionados
+         - resultado: Variable creada para mostrar en pantalla, pizza elegida, precio pizza,numero de ingredientes
+          y la suma final.
+
+         
+        */
         function calcularPrecioTotal(){
             let pizzas = document.getElementsByName("masa");
             let pizzaElegida;
